a11a7744119d439857b42c315ef6c351557aabcf
Manan Jain <manan09mj@gmail.com>
2021-01-26 02:17:52 +0530

    spinner.js
    while the data is fetching search.js file will render a spinner from this file

---
+++ src/components/spinner.js
@@ -0,0 +1,5 @@
+import {Spinner} from "react-bootstrap";
+
+const Spin = () => <Spinner style={{position: "fixed", top: "50%", left: "50%"}} animation="border" variant="dark" />
+
+export default Spin;
